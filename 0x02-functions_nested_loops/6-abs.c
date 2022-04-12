326359ef0b681e15256876223d1053275ef99d32
Kafayat6 <101401730+Kafayat6@users.noreply.github.com>
2022-04-12 21:43:01 +0100

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,1 +1,17 @@
-f
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integrer
+ *
+ * @n: the int to check
+ *
+ * Return: the absolute value of int
+ *
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
