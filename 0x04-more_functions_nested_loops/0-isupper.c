aad92cb3ee56379661a45c9875e3d65f99dc7462
Kafayat6 <101401730+Kafayat6@users.noreply.github.com>
2022-04-14 20:46:59 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,1 +1,20 @@
-f
+#include "main.h"
+
+/**
+ * _isupper - Return 1 when c is uppercase and 0 otherwise
+ * @c: Takes integer variable c
+ *
+ * Return: void
+ */
+
+int _isupper(int c)
+{
+	if (c > 64 && c < 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
