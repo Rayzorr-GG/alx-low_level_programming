2e85c80cebc36002925fb14c88aa5553e1befd7d
Kafayat6 <101401730+Kafayat6@users.noreply.github.com>
2022-04-20 20:34:22 +0100

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,20 @@
-f
+#include "main.h"
+
+/**
+ * print_numbers - This function print numbers
+ *
+ * return: nothing
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 48;
+	while (i < 58)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
