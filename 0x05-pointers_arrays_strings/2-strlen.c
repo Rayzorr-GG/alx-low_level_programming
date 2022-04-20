e41dde0d8dbf30d0e6d4065d666ffc0d20e5a01d
Kafayat6 <101401730+Kafayat6@users.noreply.github.com>
2022-04-20 20:33:42 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,16 @@
-f
+#include "main.h"
+
+/**
+ * mul - Multiply two integers
+ * @a: first integer
+ * @b: Second integer
+ * Return: the result ofthe multiplication
+ */
+
+int mul(int a, int b)
+{
+	int mult;
+
+	mult = (a * b);
+	return (mult);
+}
