774d9a3c3ba3a5cccaa2797c97e497b5e78d0000
Kafayat6 <101401730+Kafayat6@users.noreply.github.com>
2022-05-11 22:03:42 +0100

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,1 +1,22 @@
-g
+#include <stdio.h>
+#include <stdlib.h>
+#define FFILE __FILE__
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; FFILE[i] != '\0'; i++)
+	{
+		putchar(FFILE[i]);
+	}
+	putchar(10);
+	return (0);
+}
+
