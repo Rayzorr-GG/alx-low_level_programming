0934ef4bbd320706a606096b4c48ed3e4548cd2e
Kafayat6 <101401730+Kafayat6@users.noreply.github.com>
2022-05-15 22:51:46 +0100

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,7 +1,31 @@
+#include "3-calc.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include "3-calc.h" 
-/** * get_op_func - Selects the correct function to perform the operation 
-* 
-* @s: Operator passed first argument * Return: A pointer to the function that corresponds to the operator */ 
-int (*get_op_func(char *s))(int, int){ op_t ops[] = { {"+", op_add}, {"-", op_sub}, {"*", op_mul}, {"/", op_div}, {"%", op_mod}, {NULL, NULL}, }; int i; i = 0; while ((ops[i].op != NULL) && *(ops[i].op) != *s) i++; return (ops[i].f);}   
+#include <string.h>
+
+/**
+ * get_op_func - Compares function with pointer and returns the equal.
+ * @s: points a char
+ * Return: Returns the function and the operator when equal
+**/
+
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+
+int i = 0;
+
+while (ops[i].op != NULL)
+{
+if (!strcmp(ops[i].op, s))
+return (ops[i].f);
+i++;
+}
+return (NULL);
+}
